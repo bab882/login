fab5f90d973c92c9a58b03e1c018d3436d5e3508
bab882 <chanitebrandon@gmail.com>
2024-11-03 22:32:15 +0100

    commit

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-// const modal = document.querySelector(".modal");
-// const btn = document.getElementById("modal");
-// const closeBtn = document.getElementById("close");
+const modal = document.querySelector(".modal");
+const btn = document.getElementById("modal");
+const closeBtn = document.getElementById("close");
 
 
-// btn.onclick = function () {
-//   modal.style.display = "flex";
-//   btn.style.display = "none";
-// };
+btn.onclick = function () {
+  modal.style.display = "flex";
+  btn.style.display = "none";
+};
 
-// closeBtn.onclick = function () {
-//   modal.style.display = "none";
-//   btn.style.display = "inline-block";
-// };
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+  btn.style.display = "inline-block";
+};
 
 
-// window.onclick = function (event) {
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//     btn.style.display = "inline-block";
-//   }
-// };
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    btn.style.display = "inline-block";
+  }
+};
