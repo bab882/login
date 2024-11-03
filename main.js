6c397432f6e96ade1afcee7a99fb3dca7c22c2ae
bab882 <chanitebrandon@gmail.com>
2024-11-03 21:22:46 +0100

    commit css

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-// const login = document.querySelector('submit');
+const modal = document.querySelector(".modal");
+const btn = document.getElementById("modal");
+const closeBtn = document.getElementById("close");
 
+
+btn.onclick = function () {
+  modal.style.display = "flex";
+  btn.style.display = "none";
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+  btn.style.display = "inline-block";
+};
+
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    btn.style.display = "inline-block";
+  }
+};
